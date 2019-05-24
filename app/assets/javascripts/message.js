31a41297020d1a3bd5c11489db032c453b1f8554
dai0204 <nandai0204@gmail.com>
2019-05-24 19:55:50 +0900

    訂正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,46 +1,27 @@
-function buildHTML(message){
-  if ( message.image ) {
-    var html =
-     `<div class="message" data-message-id=${message.id}>
+$(function() {
+  function buildHTML(message){
+    image = (message.image) ? `<img src=${message.image} class="image">` : "";
+    var html = 
+     `<div class="message">
         <div class="upper-message">
           <div class="upper-message__user-name">
             ${message.user_name}
           </div>
           <div class="upper-message__date">
-            ${message.date}
+            ${message.created_at}
           </div>
         </div>
         <div class="lower-message">
           <p class="lower-message__content">
             ${message.content}
           </p>
-        </div>
-        <asset_path src=${message.image} >
-      </div>`
-    return html;
-  } else {
-    var html =
-     `<div class="message" data-message-id=${message.id}>
-        <div class="upper-message">
-          <div class="upper-message__user-name">
-            ${message.user_name}
-          </div>
-          <div class="upper-message__date">
-            ${message.date}
-          </div>
-        </div>
-        <div class="lower-message">
-          <p class="lower-message__content">
-            ${message.content}
-          </p>
+        <img src ="${message.image}">
         </div>
       </div>`
     return html;
-  };
-}
+  }
 
-$(function(){
-  $('#new_comment').on('submit', function(e){
+  $('.new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -52,15 +33,16 @@ $(function(){
       processData: false,
       contentType: false
     })
+    
     .done(function(data){
       var html = buildHTML(data);
-      $('form')[0].reset();
       $('.messages').append(html);
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast'); 
+      $(".form__submit").prop('disabled', false);
+      $('.message').animate({scrollTop: $('.message')[0].scrollHeight}, 'fast');
+      $('form')[0].reset();
     })
     .fail(function(){
       alert('error');
-    });
-    return false;
-   });
+    })
+  })
 });
