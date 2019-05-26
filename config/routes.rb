0930c28e02b83b13fdf8f3aa281c38a5b1b92a28
dai0204 <nandai0204@gmail.com>
2019-05-26 18:44:52 +0900

    インクリメントサーチの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'groups#index'
-  resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
-  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ root "groups#index" 
+ resources :users, only: [:index, :edit, :update] do
+   collection do
+     get 'search'
+   end
+ end
+ resources :groups, only: [:index, :new, :create, :edit, :update] do
+  resources :messages, only: [:index, :create]
+ end
 end
