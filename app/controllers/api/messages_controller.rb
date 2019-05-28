dc3dfb31a8035c8315e079da4d4ff5f0a7539022
dai0204 <nandai0204@gmail.com>
2019-05-28 17:54:11 +0900

    自動化

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,7 +2,7 @@ class Api::MessagesController < ApplicationController
   def index
     respond_to do |format|
       format.html
-      format.json{ @messages = Message.where(' id > ?', params[:id]).where(group_id:params[:group_id])}
+      format.json {@new_messages = Group.find(params[:group_id]).messages.where('id > ?', params[:id])}
     end
   end
 end
