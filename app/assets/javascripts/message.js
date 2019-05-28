8174586a3ad50ea927551f79c33c8483cec5d7cd
dai0204 <nandai0204@gmail.com>
2019-05-28 13:43:22 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,26 +1,28 @@
 $(function() {
   function buildHTML(message){
-    image = (message.image) ? `<img src=${message.image} class="image">` : "";
-    var html = 
-     `<div class="message" data-id=${message.id}>
-        <div class="upper-message">
-          <div class="upper-message__user-name">
-            ${message.user_name}
+    var image = message.image.url ? `<img src=${message.image.url} >` : ''
+      var html =
+        `<div class="message" data-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>        
+            <div class="upper-message__date">
+              ${message.date}
+            </div>
           </div>
-          <div class="upper-message__date">
-            ${message.date}
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
           </div>
-        </div>
-        <div class="lower-message">
-          <p class="lower-message__content">
-            ${message.content}
-          </p>
           <div class=""lower-message__image">
           ${image}
-        </div>
-      </div>`
-    return html;
+          </div>
+        </div>`
+      return html;
   }
+
 
   $('.new_message').on('submit', function(e){
     e.preventDefault();
@@ -34,18 +36,17 @@ $(function() {
       processData: false,
       contentType: false
     })
-    
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $(".form__submit").prop('disabled', false);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       $('form')[0].reset();
     })
     .fail(function(){
       alert('error');
-    })
-  })
+    });
+    return false;
+  });
 
 
   var reloadMessages = function() {
@@ -73,26 +74,3 @@ $(function() {
   };
   setInterval(reloadMessages, 5000);
 });
-
-
-
-
-  // var reloadMessages = function() {
-  //   //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-  //   last_message_id = ※※※
-  //   $.ajax({
-  //     //ルーティングで設定した通りのURLを指定
-  //     url: ※※※,
-  //     //ルーティングで設定した通りhttpメソッドをgetに指定
-  //     type: 'get',
-  //     dataType: 'json',
-  //     //dataオプションでリクエストに値を含める
-  //     data: {id: last_message_id}
-  //   })
-  //   .done(function(messages) {
-  //     console.log('success');
-  //   })
-  //   .fail(function() {
-  //     console.log('error');
-  //   });
-  // };
